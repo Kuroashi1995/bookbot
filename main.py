a5333b86c4c8a9ba57479a50b02113ee0693a9f7
Kuroashi1995 <kuroashi.prog@gmail.com>
2025-02-09 23:33:51 -0300

    Program now counts books words

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+def count_words(string):
+    words_array = string.split()
+    return len(words_array)
+
+
 def main():
     with open("books/frankestein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        number_of_words = count_words(file_contents)
+        print(number_of_words)
 
 main()
