10f5049812ae98b771458fcd4abdbd45e6ff9c8a
Kuroashi1995 <kuroashi.prog@gmail.com>
2025-02-10 01:21:34 -0300

    Printing frankestein report

--- main.py
+++ main.py
@@ -2,22 +2,34 @@ def count_words(string):
     words_array = string.split()
     return len(words_array)
 
-def get_characters(string):
-    unique_chars = set()
+def sort_on(dict):
+    return dict["repeats"]
+
+def get_letters(string):
+    valid_letters = 'abcdefghijklmnopqrstuvwxyz'
     counter = {}
+    as_list = []
     for character in string:
-        if character.lower() in unique_chars:
+        if character.lower() in valid_letters and character.lower() in counter:
             counter[character.lower()] += 1
-        else:
-            unique_chars.add(character.lower())
+        elif character.lower() in valid_letters:
             counter[character.lower()] = 1
-    return counter
+    for element in counter:
+        as_list.append({"letter": element, "repeats": counter[element]})
+
+    as_list.sort(reverse=True, key=sort_on)
+    return as_list
 
 def main():
     with open("books/frankestein.txt") as f:
         file_contents = f.read()
         number_of_words = count_words(file_contents)
-        characters = get_characters(file_contents)
-        print(characters)
+        letters = get_letters(file_contents)
+
+        print("Printing report for books/frankestein.txt")
+        print(f"The book contains {number_of_words} words.")
+        for i in range(0, len(letters)):
+            print(f"The letter '{letters[i]['letter']}' was found {letters[i]['repeats']} times.")
+        print("End of report")
 
 main()
